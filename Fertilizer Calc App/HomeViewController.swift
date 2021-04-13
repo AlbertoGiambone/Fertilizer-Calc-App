ae85af6c41e60b41d3e1d0c364c4aeb503f4fc74
alberto.giambone@gmail.com <alberto.giambone@gmail.com>
2021-04-13 15:45:21 +0200

    starting Firestore and new app version

---
+++ Fertilizer Calc App/HomeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HomeViewController.swift
+//  Fertilizer Calc App
+//
+//  Created by Alberto Giambone on 13/04/21.
+//
+
+import UIKit
+import Firebase
+import FirebaseUI
+
+class HomeViewController: UIViewController, FUIAuthDelegate {
+
+    
+    func showLoginVC() {
+        let autUI = FUIAuth.defaultAuthUI()
+        let providers = [FUIOAuth.appleAuthProvider()]
+        
+        autUI?.providers = providers
+        
+        let autViewController = autUI!.authViewController()
+        autViewController.modalPresentationStyle = .fullScreen
+        self.present(autViewController, animated: true, completion: nil)
+    }
+    
+    func showUserInfo(user:User) {
+        
+        print("USER.UID: \(user.uid)")
+        UserDefaults.standard.setValue(user.uid, forKey: "userInfo")
+    }
+    
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if let user = authDataResult?.user {
+            print("GREAT!!! You Are Logged in as \(user.uid)")
+            UserDefaults.standard.setValue(user.uid, forKey: "userInfo")
+        }
+    }
+    
+    
+    
+    //MARK: Lifecycle
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            if let user = user {
+                self.showUserInfo(user:user)
+            } else {
+                self.showLoginVC()
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+    
+}
