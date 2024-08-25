704e9a9a7d66a6e32d89edcc19dfea7445b310f6
Alberto Giambone <alberto.giambone@gmail.com>
2024-08-25 15:00:28 +0200

    Add files via upload

---
+++ iPadSettings_Tv_Cell.swift
@@ -0,0 +1,31 @@
+//
+//  iPadSettings_Tv_Cell.swift
+//  Fertilizer Calc App
+//
+//  Created by Alberto Giambone on 20/05/24.
+//
+
+import UIKit
+
+class iPadSettings_Tv_Cell: UITableViewCell {
+
+    //MARK: Connection
+    
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    @IBOutlet weak var LogoIMG: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
